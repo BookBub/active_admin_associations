4e32eaeb116d42c4376eb78c783118f6390d7ebc
Lisa Plesko <plesko.l@gmail.com>
2017-07-11 09:57:50 -0400

    Update for Rails 5 upgrade (#11)
    * removing rails dep & most dependencies
    * removing anything formtastic-ish

--- lib/activeadmin_associations.rb
+++ lib/activeadmin_associations.rb
@@ -2,8 +2,6 @@ require 'active_support'
 require 'activeadmin'
 require 'active_admin_associations/version'
 require 'active_admin_associations/active_admin_extensions'
-require 'formtastic/token_input_default_for_association'
-require 'formtastic/inputs/token_input'
 
 module ActiveAdminAssociations
   extend ActiveSupport::Autoload
